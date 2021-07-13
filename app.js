c65cbc31ea3f0fa12b7ec18c91c897d4097d633f
andry705ser <anzhuzha705@gmail.com>
2021-07-13 12:23:34 +0300

    Added app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+name1 = prompt('Введите ваше имя?', 'Andrew');
+//let name1 = ('Введите ваше имя?');
+alert(name1);
+let name2 = ('Введите вашу фамилию?');
+alert(name2);
+let name3 = ('Введите ваш возвраст?');
+//alert = name3
